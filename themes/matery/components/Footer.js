0e3cd2d8631cd1fda38c1d919dbf6434610b46a6
mairn <162576211+Marinff@users.noreply.github.com>
2025-02-23 10:46:23 +0800

    Update Footer.js

--- themes/matery/components/Footer.js
+++ themes/matery/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = ({ title }) => {
   })()
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-white text-gray-800 justify-center text-center m-auto w-full leading-6 dark:text-gray-100 text-sm p-6'>
+    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-red text-gray-800 justify-center text-center m-auto w-full leading-6 dark:text-gray-100 text-sm p-6'>
       {/* <DarkModeButton/> */}
       <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
       <span>
